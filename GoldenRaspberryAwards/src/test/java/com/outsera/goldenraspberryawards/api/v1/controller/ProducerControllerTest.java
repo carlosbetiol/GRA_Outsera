bf3f42e82ecc93ff8d5e1e257ec9d05bf6fc2aab
Carlos Betiol <carlos@betiol.dev>
2024-09-10 12:03:23 -0300

    test and deploy

--- GoldenRaspberryAwards/src/test/java/com/outsera/goldenraspberryawards/api/v1/controller/ProducerControllerTest.java
+++ GoldenRaspberryAwards/src/test/java/com/outsera/goldenraspberryawards/api/v1/controller/ProducerControllerTest.java
@@ -3,7 +3,11 @@ package com.outsera.goldenraspberryawards.api.v1.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.outsera.goldenraspberryawards.api.v1.model.response.AwardBorderResponseDTO;
 import com.outsera.goldenraspberryawards.api.v1.model.response.AwardIntervalResponseDTO;
-import com.outsera.goldenraspberryawards.domain.service.DatabasePopulateService;
+import com.outsera.goldenraspberryawards.domain.model.Movie;
+import com.outsera.goldenraspberryawards.domain.model.MovieAward;
+import com.outsera.goldenraspberryawards.domain.model.Producer;
+import com.outsera.goldenraspberryawards.domain.model.Studio;
+import com.outsera.goldenraspberryawards.domain.service.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +20,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import java.util.List;
 
@@ -26,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @TestPropertySource("/application-test.yml")
 @ActiveProfiles( "test")
+@Transactional
 public class ProducerControllerTest {
 
     @Autowired
@@ -37,7 +43,6 @@ public class ProducerControllerTest {
     }
 
     @Test
-    @Transactional
     public void shouldStatusOkResponseToGetTopProducers() throws Exception {
 
         mockMvc.perform(MockMvcRequestBuilders
@@ -51,7 +56,6 @@ public class ProducerControllerTest {
     }
 
     @Test
-    @Transactional
     public void shouldMatchTopProducer(@Autowired ObjectMapper objectMapper) throws Exception {
 
         String responseContent = mockMvc.perform(MockMvcRequestBuilders
@@ -88,5 +92,38 @@ public class ProducerControllerTest {
         assertEquals(expected, actualResponse);
 
     }
+
+    @Test
+    public void shouldMatchMoviesRowsCount(@Autowired MovieService movieService) throws Exception {
+
+        List<Movie> movies = movieService.findAll();
+        Assert.isTrue(movies.size() == 51, String.format("Movies count should be 51, but %d was found", movies.size()));
+
+    }
+
+    @Test
+    public void shouldMatchStudiosRowsCount(@Autowired StudioService studioService) throws Exception {
+
+        List<Studio> studios = studioService.findAll();
+        Assert.isTrue(studios.size() == 27, String.format("Studios count should be 27, but %d was found", studios.size()));
+
+    }
+
+    @Test
+    public void shouldMatchProducersRowsCount(@Autowired ProducerService producerService) throws Exception {
+
+        List<Producer> producers = producerService.findAll();
+        Assert.isTrue(producers.size() == 132, String.format("Producers count should be 132, but %d was found", producers.size()));
+
+    }
+
+    @Test
+    public void shouldMatchMoviesAwardedRowsCount(@Autowired MovieAwardService movieAwardService) throws Exception {
+
+        List<MovieAward> movieAwardedList = movieAwardService.findAll();
+        Assert.isTrue(movieAwardedList.size() == 19, String.format("Movie Awards count should be 19, but %d was found", movieAwardedList.size()));
+
+    }
+
 
 }
