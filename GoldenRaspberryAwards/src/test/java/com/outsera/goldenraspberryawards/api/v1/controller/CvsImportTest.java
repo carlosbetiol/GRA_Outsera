aef492388ed52b16d131c6d42ec7d62f5d9b4bb0
Carlos Betiol <carlos@betiol.dev>
2024-09-10 04:18:37 -0300

    adjustments

--- GoldenRaspberryAwards/src/test/java/com/outsera/goldenraspberryawards/api/v1/controller/CvsImportTest.java
+++ GoldenRaspberryAwards/src/test/java/com/outsera/goldenraspberryawards/api/v1/controller/CvsImportTest.java
@@ -1,4 +1,4 @@
-package com.outsera.goldenraspberryawards;
+package com.outsera.goldenraspberryawards.api.v1.controller;
 
 import com.outsera.goldenraspberryawards.domain.model.Movie;
 import com.outsera.goldenraspberryawards.domain.model.MovieAward;
@@ -22,7 +22,7 @@ import java.util.List;
 @AutoConfigureMockMvc
 @TestPropertySource("/application-test.yml")
 @ActiveProfiles( "test")
-public class CvsImportIT {
+public class CvsImportTest {
 
     @Autowired
     private MockMvc mockMvc;
